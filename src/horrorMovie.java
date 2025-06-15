2e15b41d4879f34870db2ca887f4d57886d3ab14
jmussleh <Jmussleh@gmail.com>
2025-06-15 12:12:52 -0400

    Added all classes for my MVC paradigm. Also added all necessary fields to my horrorMovie object class.

--- src/horrorMovie.java
+++ src/horrorMovie.java
@@ -1,2 +1,11 @@
 public class horrorMovie {
+    private String title;
+    private String director;
+    private int releaseYear;
+    private int runtimeMinutes;
+    private String streamingPlatform;
+    private String rating;
+    private String tags;
+    private String dateWatched;
+
 }
