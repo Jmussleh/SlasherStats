ed5690c60a3c71c0cbdfbc4f5875c2ba2a0ce9b2
jmussleh <Jmussleh@gmail.com>
2025-06-15 17:11:07 -0400

    Added my global variables

--- src/slasherStatsApp.java
+++ src/slasherStatsApp.java
@@ -1,2 +1,11 @@
+//Imports all classes from the java package util.
+import java.util.*;
+
 public class slasherStatsApp {
+    //scanner object for reading user input from the console
+    static Scanner sc = new Scanner(System.in);
+    //Creates a list to show horror movie objects that are created by the user
+    static List<horrorMovie> movies = new ArrayList<>();
+    //starts account points at 0 as a starting point
+    static int accountPoints = 0;
 }
