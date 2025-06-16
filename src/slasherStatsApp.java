eee7a1f36329b972f2a6895ef3471ef0bee93744
jmussleh <Jmussleh@gmail.com>
2025-06-16 19:25:22 -0400

    Added logic to begin the program

--- src/slasherStatsApp.java
+++ src/slasherStatsApp.java
@@ -8,4 +8,16 @@ public class slasherStatsApp {
     static List<horrorMovie> movies = new ArrayList<>();
     //starts account points at 0 as a starting point
     static int accountPoints = 0;
+
+    //starts the program and controls the loop
+    public static void main(String[] args) {
+        boolean running = true;
+        while (running) {
+            //Displays the menu and reads the user choice
+            displayMenu();
+            String input = sc.nextLine();
+            //Determines the action
+            switch (input) {}
+        }
+    }
 }
